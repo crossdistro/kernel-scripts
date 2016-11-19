e3a20373293e050272860f6c2be60a6af7f346b5
Pavel Šimerda <pavlix@pavlix.net>
2016-11-20 00:53:40 +0100

    fix description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import nose
 setup(
     name="kernel-tools",
     version="0.0.1",
-    description="Automatic kernel configuration and building in Gentoo and building in Gentoo",
+    description="Automatic kernel configuration and building in Gentoo",
     scripts=["user-config.py"],
     packages=find_packages(),
     testssuite=nose.collector
